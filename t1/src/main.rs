c1cbe7862b49c4caffed3e3c47731e21f887ef9d
yy20190811 <yy20190811@126.com>
2023-04-30 16:13:43 +0800

    学习格式化输出的格式，总体游，参数序号，参数名称，参数进制，参数格式（左右不足的添加）

--- t1/src/main.rs
+++ t1/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
+    //This is yy's first rust project
     println!("Hello, world!");
+    /*so, muilt lines
+    an so other
+    */
+    println!("{} is days", 30);
+    //参数编号
+    println!("{}, this is {0}, and this is {1}, {0} and {1} ", 50, 100);
+    //参数名称
+    println!("{s1},{s2},{s3}", s1="s3", s3="100", s2="s1");
+    //参数的进制
+    println!("base10:{0},base8:{0:o},base2:{0:b}, base16:{0:x}, BASE16:{0:X}", 1000);
+
+
+    //左右填充
+    println!("{number:>5}", number=1);
+    println!("{number:0<5}", number=1);
+    println!("{number:0>width$}", number=1, width=5);
+
+    //下面的错误要被编译器捕捉到
+    //println!("My name is {0}, {1} {0}", "Bond");
+
+
+    let number: f64 = 1.0;
+    let width: usize = 5;
+    println!("{number:9<width$}");
 }
